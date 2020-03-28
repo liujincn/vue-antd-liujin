0f2a184c431249f03854224ac866fa5838ee9204
liujincn <liujincn@qq.com>
2020-03-28 16:46:29 +0800

    时间组件

---
+++ src/components/TimePicker.vue
@@ -0,0 +1,26 @@
+<template>
+    <a-time-picker
+            @change="changeTime"
+            v-model="time"
+            format="HH:mm:ss"
+            placeholder="选择时间">
+    </a-time-picker>
+</template>
+
+<script>
+    import moment from 'moment'
+    export default {
+        data() {
+            return {
+                time: null,
+            }
+        },
+        methods: {
+            moment,
+            changeTime(value) {
+                const time = moment(value).format("HH:mm:ss")
+                this.$emit('changeTime', time)
+            }
+        }
+    }
+</script>
